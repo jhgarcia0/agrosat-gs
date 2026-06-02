90a25cd7942c3f757a214e62ca7cae8074dfd143
LuanRodrigues15 <luan15008090@gmail.com>
2026-06-02 14:30:00 -0300

    add analysis viewmodel

---
+++ lib/presentation/viewmodels/analysis_viewmodel.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/foundation.dart';
+import '../../data/models/analysis_result.dart';
+import '../../data/models/region.dart';
+import '../../data/repositories/analysis_repository.dart';
+
+enum ViewState { idle, loading, success, error }
+
+class AnalysisViewModel extends ChangeNotifier {
+  final AnalysisRepository _repository;
+
+  ViewState _state = ViewState.idle;
+  AnalysisResult? _result;
+  String? _errorMessage;
+
+  AnalysisViewModel(this._repository);
+
+  ViewState get state => _state;
+  AnalysisResult? get result => _result;
+  String? get errorMessage => _errorMessage;
+
+  Future<void> analyze(Region region) async {
+    _state = ViewState.loading;
+    _errorMessage = null;
+    notifyListeners();
+
+    try {
+      _result = await _repository.analyze(region);
+      _state = ViewState.success;
+    } catch (_) {
+      _errorMessage = 'Erro ao buscar dados.';
+      _state = ViewState.error;
+    }
+
+    notifyListeners();
+  }
+}
