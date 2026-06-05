a1f42fb79493da92482d2c48b19a8407f7686144
João Henrique <joao.henrique.dev@gmail.com>
2026-06-05 16:20:00 -0300

    expande modelo com mais 4 regioes agricolas

--- lib/data/models/region.dart
+++ lib/data/models/region.dart
@@ -48,5 +48,37 @@ class Region {
       longitude: -50.93,
       description: 'Grande polo de soja e milho do Cerrado.',
     ),
+    Region(
+      id: 'londrina_pr',
+      name: 'Londrina',
+      state: 'PR',
+      latitude: -23.31,
+      longitude: -51.17,
+      description: 'Referência em pesquisa agrícola da Embrapa Soja.',
+    ),
+    Region(
+      id: 'barreiras_ba',
+      name: 'Barreiras',
+      state: 'BA',
+      latitude: -12.15,
+      longitude: -44.99,
+      description: 'MATOPIBA: nova fronteira agrícola do Cerrado nordestino.',
+    ),
+    Region(
+      id: 'uberlandia_mg',
+      name: 'Uberlândia',
+      state: 'MG',
+      latitude: -18.91,
+      longitude: -48.27,
+      description: 'Polo de soja e milho do Triângulo Mineiro.',
+    ),
+    Region(
+      id: 'dourados_ms',
+      name: 'Dourados',
+      state: 'MS',
+      latitude: -22.22,
+      longitude: -54.81,
+      description: 'Grande produtor de soja e milho do Mato Grosso do Sul.',
+    ),
   ];
 }
